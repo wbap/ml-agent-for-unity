7ad6dff02f644019df1360bf084b5c7f460b812d
Masayoshi Nakamura <mchmasa@gmail.com>
2016-04-05 13:28:54 +0900

    bugfix

--- python-agent/server.py
+++ python-agent/server.py
@@ -44,7 +44,7 @@ class AgentServer(WebSocket):
 
         dat = msgpack.unpackb(payload)
         image = Image.open(io.BytesIO(bytearray(dat['image'])))
-        reward = a
+        reward = dat['reward']
         end_episode = dat['endEpisode']
         # image.save(str(self.cycle_counter) + ".png")
 
