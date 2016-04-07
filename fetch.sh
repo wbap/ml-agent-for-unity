f5072f5f5b0f64a134b86b8ecc51693439c62be8
Masayoshi Nakamura <mchmasa@gmail.com>
2016-04-08 01:52:11 +0900

    modify fetch

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "download caffemodel..."
-curl -o python-agent/bvlc_alexnet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
-curl -o python-agent/ilsvrc_2012_mean.npy https://github.com/BVLC/caffe/raw/master/python/caffe/imagenet/ilsvrc_2012_mean.npy
+wget -o python-agent/bvlc_alexnet.caffemodel http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel
+wget -o python-agent/ilsvrc_2012_mean.npy https://github.com/BVLC/caffe/raw/master/python/caffe/imagenet/ilsvrc_2012_mean.npy
 
